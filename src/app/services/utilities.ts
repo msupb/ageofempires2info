0013cd9d87b5c8caec974e95718bd642d52d58b9
msupb <eriksson.marcus15@gmail.com>
2020-11-06 00:06:11 +0100

    Minor refactor in utilities

--- src/app/services/utilities.ts
+++ src/app/services/utilities.ts
@@ -5,11 +5,11 @@ class Utilities {
 
         let regex2 = /^\/|\/$/g;
 
-        let string1: string = url.match(regex1)[0];
+        let output: string = url.match(regex1)[0];
 
-        let string2: string = string1.replace(regex2, '');
+        output = output.replace(regex2, '');
 
-        return string2.split('/');
+        return output.split('/');
     }
 
 }
